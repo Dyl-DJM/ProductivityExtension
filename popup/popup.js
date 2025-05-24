ce7fe186d47975cbd58c892ef0e1e398dae2fe41
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-05-24 18:08:48 +0200

    Added some doc

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,9 @@
+// div containing the clock display
 var clockDiv = document.getElementById("clock")
 
+/**
+ * Updates the clocks display in the div.
+ */
 function updateClock(){
 	const now = new Date();
 	let hours = now.getHours().toString().padStart(2, '0');
@@ -8,6 +12,8 @@ function updateClock(){
 	clockDiv.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
+// Initial call when the script is loaded
 updateClock()
 
+// Clock is updated once per second
 setInterval(updateClock, 1000)
