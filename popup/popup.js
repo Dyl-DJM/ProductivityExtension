3e3db22e3caf193738a6a9f1d7c0b3d3dfdbf96d
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-05-24 17:40:46 +0200

    Made a clock extension on the popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,13 @@
-console.log("This is a popup!")
+var clockDiv = document.getElementById("clock")
+
+function updateClock(){
+	const now = new Date();
+	let hours = now.getHours().toString().padStart(2, '0');
+	let minutes = now.getMinutes().toString().padStart(2, '0');
+	let seconds = now.getSeconds().toString().padStart(2, '0');
+	clockDiv.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+updateClock()
+
+setInterval(updateClock, 1000)
